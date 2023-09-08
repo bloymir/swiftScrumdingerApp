ac7e7e984c2bd8e03e91bdd9c2fa05a27b3d947c
Nelson Tapia <ntapiapinto@gmail.com>
2023-09-07 23:35:43 -0300

    Se agrega LabelStyle y se agrega en CardView

--- swiftScrumdingerApp/Views/CardView.swift
+++ swiftScrumdingerApp/Views/CardView.swift
@@ -13,7 +13,7 @@ struct CardView: View {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
-                    .padding(.trailing, 20)
+                    .labelStyle(.trailingIcon)
             }
             .font(.caption)
         }
