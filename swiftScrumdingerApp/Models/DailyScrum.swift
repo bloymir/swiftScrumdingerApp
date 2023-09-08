d662d3a692a243396cce6a1257154c5f9c18b690
Nelson Tapia <ntapiapinto@gmail.com>
2023-09-08 00:54:28 -0300

    Se agrega Struct Attende para entregar Identificable

--- swiftScrumdingerApp/Models/DailyScrum.swift
+++ swiftScrumdingerApp/Models/DailyScrum.swift
@@ -5,7 +5,7 @@ import Foundation
 struct DailyScrum: Identifiable {
     let id: UUID
     var title: String
-    var attendees: [String]
+    var attendees: [Attendee]
     var lengthInMinutes: Int
     var theme: Theme
     
@@ -13,10 +13,23 @@ struct DailyScrum: Identifiable {
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
         self.id = id
         self.title = title
-        self.attendees = attendees
+        self.attendees = attendees.map { Attendee(name: $0) }
         self.lengthInMinutes = lengthInMinutes
         self.theme = theme
     }
+}
+
+extension DailyScrum {
+    struct Attendee: Identifiable {
+        let id: UUID
+        var name: String
+        
+        init(id: UUID = UUID(), name: String) {
+            self.id = id
+            self.name = name
+        }
+    }
+    
 }
 
 extension DailyScrum {
