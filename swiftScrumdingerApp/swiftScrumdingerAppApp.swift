b009cbc30f8224345dc3729023eca873aba5e222
Nelson Tapia <ntapiapinto@gmail.com>
2023-09-08 00:09:18 -0300

    Se agrega ScrumsView como vista principal

--- swiftScrumdingerApp/swiftScrumdingerAppApp.swift
+++ swiftScrumdingerApp/swiftScrumdingerAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct swiftScrumdingerAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ScrumsView(scrums: DailyScrum.sampleData)
         }
     }
 }
