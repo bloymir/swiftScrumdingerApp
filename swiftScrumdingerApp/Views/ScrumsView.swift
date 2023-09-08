26b9a2b4883168b7c477ac7fa302dd3120f4b53d
Nelson Tapia <ntapiapinto@gmail.com>
2023-09-08 00:01:54 -0300

    Se agrega Identificable a DailyScrum

--- swiftScrumdingerApp/Views/ScrumsView.swift
+++ swiftScrumdingerApp/Views/ScrumsView.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct ScrumsView: View {
     let scrums: [DailyScrum]
     var body: some View {
-        List(scrums, id: \.title){ scrum in
+        List(scrums){ scrum in
             CardView(scrum: scrum)
                 .listRowBackground(scrum.theme.mainColor)
         }
