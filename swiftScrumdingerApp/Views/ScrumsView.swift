238a86dd2ed1ae3d9c70d8cbdfd3eb3de6ed5be2
Nelson Tapia <ntapiapinto@gmail.com>
2023-09-07 23:57:30 -0300

    Se agrega ScrumsView con una Lista para mostrar CardView

---
+++ swiftScrumdingerApp/Views/ScrumsView.swift
@@ -0,0 +1,18 @@
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums, id: \.title){ scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
