6e318350d46269a7eb6cf92d116b39f06f3d984b
Nelson Tapia <ntapiapinto@gmail.com>
2023-09-07 23:51:58 -0300

    se agrega accesibilidad a CardView

--- swiftScrumdingerApp/Views/CardView.swift
+++ swiftScrumdingerApp/Views/CardView.swift
@@ -8,9 +8,11 @@ struct CardView: View {
         VStack (alignment: .leading){
             Text(scrum.title)
                 .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack{
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
                     .labelStyle(.trailingIcon)
