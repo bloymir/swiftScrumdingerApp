f08368d2aca5e7804e18ccf8a524ba54cafd064a
Nelson Tapia <ntapiapinto@gmail.com>
2023-09-07 23:19:38 -0300

    Se crea CardView

--- swiftScrumdingerApp/Views/CardView.swift
+++ swiftScrumdingerApp/Views/CardView.swift
@@ -5,7 +5,16 @@ struct CardView: View {
     let scrum : DailyScrum
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (alignment: .leading){
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack{
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+            }
+        }
     }
 }
 
@@ -15,5 +24,6 @@ struct CardView_Previews: PreviewProvider {
     static var previews: some View {
         CardView(scrum: scrum)
             .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
     }
 }
