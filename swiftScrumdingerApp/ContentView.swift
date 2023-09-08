3c2293c646353f56a010f654218a6ffe236a28f7
Nelson Tapia <ntapiapinto@gmail.com>
2023-09-07 21:37:58 -0300

    Initial Commit

---
+++ swiftScrumdingerApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swiftScrumdingerApp
+//
+//  Created by nelson tapia on 07-09-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftScrumdingerApp/swiftScrumdingerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftScrumdingerAppApp.swift
+//  swiftScrumdingerApp
+//
+//  Created by nelson tapia on 07-09-23.
+//
+
+import SwiftUI
+
+@main
+struct swiftScrumdingerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
