abdbd15b11922d119c521ce0a7d4760894d968c5
Nelson Tapia <ntapiapinto@gmail.com>
2023-09-11 23:29:08 -0300

    Se crea sección de asistentes con la opción de borrar y agregar nuevos asistentes Ademas se refactoriza el código para orden

--- swiftScrumdingerApp/Views/DetailEditView.swift
+++ swiftScrumdingerApp/Views/DetailEditView.swift
@@ -3,19 +3,51 @@ import SwiftUI
 
 struct DetailEditView: View {
     @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
     
     var body: some View {
         Form{
-            Section(header: Text("Información de la reunión")) {
-                TextField("Titulo", text: $scrum.title)
-                HStack{
-                    Slider(value: $scrum.lengthInMinutesAsADouble, in: 5...30, step: 1) {
-                            Text("Duración")
-                    }
-                    Spacer()
-                    Text("\(scrum.lengthInMinutes) minutos")
+            title
+            asistentes
+            
+        }
+    }
+    
+    var title: some View {
+        Section(header: Text("Información de la reunión")) {
+            TextField("Titulo", text: $scrum.title)
+            HStack{
+                Slider(value: $scrum.lengthInMinutesAsADouble, in: 5...30, step: 1) {
+                    Text("Duración")
                 }
+                Spacer()
+                Text("\(scrum.lengthInMinutes) minutos")
             }
+        }
+    }
+    
+    var asistentes: some View{
+        Section(header: Text("Asistentes")) {
+            ForEach(scrum.attendees) { atendee in
+                Text(atendee.name)
+            }.onDelete { index in
+                scrum.attendees.remove(atOffsets: index)
+            }
+            HStack{
+                TextField("Nuevo asistente", text: $newAttendeeName)
+                Button(action:{
+                    withAnimation {
+                        let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                        scrum.attendees.append(attendee)
+                        newAttendeeName = ""
+                    }
+                }){
+                    Image(systemName: "plus.circle.fill")
+                }
+                .disabled(newAttendeeName.isEmpty ? true : false)
+
+            }
+            
         }
     }
 }
