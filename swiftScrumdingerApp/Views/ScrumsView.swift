1c0ab57232e09870c385ef442ea9f4c8bd6a441f
Nelson Tapia <ntapiapinto@gmail.com>
2023-09-21 20:09:24 -0300

    Se cambia variable Scrums a Binding en Scrums View

--- swiftScrumdingerApp/Views/ScrumsView.swift
+++ swiftScrumdingerApp/Views/ScrumsView.swift
@@ -2,11 +2,12 @@
 import SwiftUI
 
 struct ScrumsView: View {
-    let scrums: [DailyScrum]
+    @Binding var scrums: [DailyScrum]
+    
     var body: some View {
         NavigationStack{
-            List(scrums){ scrum in
-                NavigationLink(destination: DetailView(scrum: scrum)) {
+            List($scrums){ $scrum in
+                NavigationLink(destination: DetailView(scrum: $scrum)) {
                     CardView(scrum: scrum)
                         
                 }
@@ -29,6 +30,6 @@ struct ScrumsView: View {
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: DailyScrum.sampleData)
+        ScrumsView(scrums: .constant(DailyScrum.sampleData))
     }
 }
