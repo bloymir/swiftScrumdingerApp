4f38c855f692f4f85357a2c778144f2d1e99899d
Nelson Tapia <ntapiapinto@gmail.com>
2023-09-21 20:02:29 -0300

    Se cambia variable scrum a Binding

--- swiftScrumdingerApp/Views/DetailEditView.swift
+++ swiftScrumdingerApp/Views/DetailEditView.swift
@@ -2,7 +2,7 @@
 import SwiftUI
 
 struct DetailEditView: View {
-    @State private var scrum = DailyScrum.emptyScrum
+    @Binding var scrum: DailyScrum
     @State private var newAttendeeName = ""
     
     var body: some View {
@@ -58,6 +58,6 @@ struct DetailEditView: View {
 
 struct DetailEditView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailEditView()
+        DetailEditView(scrum: .constant(DailyScrum.sampleData[0]))
     }
 }
