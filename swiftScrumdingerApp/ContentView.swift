ba8aa3e7ec773f33e45867e03c2a7fa1e3063e02
Nelson Tapia <ntapiapinto@gmail.com>
2023-09-25 18:20:23 -0300

    Se extra el Header de la vista principal

--- swiftScrumdingerApp/ContentView.swift
+++ swiftScrumdingerApp/ContentView.swift
@@ -5,28 +5,13 @@ struct ContentView: View {
     
     @Binding var scrum: DailyScrum
     
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16)
                 .fill(scrum.theme.mainColor)
             VStack {
-                ProgressView(value: 10, total: 15)
-                HStack{
-                    VStack (alignment: .leading){
-                        Text("Segundos Transcurridos")
-                            .font(.caption)
-                        Label("300", systemImage: "hourglass.tophalf.fill")
-                    }
-                    Spacer()
-                    VStack (alignment: .trailing){
-                        Text("Segundos Restantes")
-                            .font(.caption)
-                        Label("600", systemImage: "hourglass.bottomhalf.fill")
-                    }
-                }
-                .accessibilityElement(children: .ignore)
-                .accessibilityLabel("Tiempo restante")
-                .accessibilityValue("10 minutos")
+               
                 Circle()
                     .strokeBorder(lineWidth: 24)
                 HStack{
---
+++ swiftScrumdingerApp/Views/MeetingHeaderView.swift
@@ -0,0 +1,50 @@
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        return Double(secondsElapsed) / Double(totalSeconds)
+    }
+    
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    
+    var body: some View {
+        VStack{
+            ProgressView(value: progress)
+            HStack{
+                VStack (alignment: .leading){
+                    Text("Segundos Transcurridos")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack (alignment: .trailing){
+                    Text("Segundos Restantes")
+                        .font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Tiempo restante")
+        .accessibilityValue("\(minutesRemaining) minutos")
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180)
+            .previewLayout(.sizeThatFits)
+    }
+}
